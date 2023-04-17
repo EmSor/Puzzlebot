5c4f7c6fff273c8222146cfc74768ac5b7acc797
AndresVDLT <75438058+AndresVDLT@users.noreply.github.com>
2023-04-16 22:50:29 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,33 +4,91 @@ import rospy
 from geometry_msgs.msg import Twist
 import numpy as np
 
-# Inicializa el nodo
-rospy.init_node('recorrido_cuadrado')
+def change_speed(linear_speed, angular_speed):
+    twist = Twist()
+    twist.linear.x = linear_speed
+    twist.angular.z = angular_speed
+    return twist
 
-# Define los comandos de movimiento
-move_forward = Twist()
-move_forward.linear.x = 0.5
-turn_left = Twist()
-turn_left.angular.z = np.pi/4
+# Initialize the node
+rospy.init_node('square_controller')
 
-# Calcula el tiempo de recorrido
-tiempo_recorrido = 20.0
+# Set the side length of the square
+side_length = 2.0
 
-# Crea el publicador para enviar los comandos de movimiento
+# Ask the user to choose an option
+print("Choose an option:")
+print("1. Enter the desired linear speed")
+print("2. Enter the desired time to complete the square path")
+option = int(input("Enter the option number (1 or 2): "))
+
+if option == 1:
+    # Get the desired linear speed from the user
+    linear_speed = float(input("Enter the desired linear speed (m/s): "))
+    # Calculate the time for each segment based on the desired linear speed
+    # Calculate the time for each segment based on the desired linear speed
+    if(linear_speed == 1):
+        segment_time = (side_length / linear_speed)
+    elif(linear_speed < 3):
+        segment_time = (side_length / linear_speed) + 1
+    elif(linear_speed < 4):
+        segment_time = (side_length / linear_speed) + 1.2
+    else:
+        segment_time = (side_length / linear_speed) + 1.5
+    # Calculate the angular speed based on the desired linear speed and side length
+    angular_speed = np.pi / 2 / segment_time
+elif option == 2:
+    # Get the desired time to complete the square path from the user
+    total_time = float(input("Enter the desired time to complete the square path (s): "))
+    # Calculate the time for each segment based on the total time
+    segment_time = (total_time - 9) / 4
+    # Calculate the linear speed based on the segment time
+    linear_speed = side_length / segment_time
+    # Calculate the angular speed based on the desired linear speed and side length
+    angular_speed = np.pi / 2
+else:
+    print("Invalid option. Exiting.")
+    exit()
+
+
+
+# Set the time for each segment of the square as a rospy.Duration object
+segment_time_duration = rospy.Duration(segment_time)
+
+# Create the publisher for the Twist messages
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
-# Espera a que el nodo de tiempo simulado este activo
-while not rospy.Time.now():
-    pass
+# Wait for the publisher to connect to the topic
+rospy.sleep(1)
 
-# Publica los comandos de movimiento para recorrer el cuadrado
-rospy.sleep(2)
-while not rospy.is_shutdown():
+# Move the robot in a square pattern
+for i in range(4):
+    # Move forward for the duration of the segment
+    move_forward = change_speed(linear_speed, 0)
     pub.publish(move_forward)
-    rospy.sleep(4.0)
-    pub.publish(turn_left)
-    rospy.sleep(2.0)
-# Detiene el robot antes de salir del nodo
-pub.publish(Twist())
-rospy.sleep(1.0)
+    rospy.sleep(segment_time_duration)
 
+    # Stop the robot
+    stop = change_speed(0, 0)
+    pub.publish(stop)
+    rospy.sleep(1.0)
+
+    # Turn left for the duration of the segment
+    turn_left = change_speed(0, angular_speed)
+    pub.publish(turn_left) 
+    if option == 1:
+        rospy.sleep(segment_time_duration)
+    else:
+        rospy.sleep(1)
+
+
+
+    # Stop the robot
+    pub.publish(stop)
+    # Stop the robot
+    pub.publish(stop)
+    rospy.sleep(1.0)
+
+# Stop the robot before exiting
+pub.publish(stop)
+rospy.sleep(1)
